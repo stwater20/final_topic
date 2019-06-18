dfcff19de04696efaa9b64c16df2af1e6d188640
陳勝舢 <54sakuramiku@gmail.com>
2019-06-18 10:58:47 +0800

    add code

--- db_process.py
+++ db_process.py
@@ -8,6 +8,7 @@ def crawlerNewData(url):
     resp = requests.get(url)
     json_dict = (resp.json())
     return json_dict
+
 #print(type(json_dict))
 #print(type(json_dict[0]))
 #print(len(json_dict))
@@ -20,20 +21,68 @@ def crawlerNewData(url):
 
 
 
-connStr = 'DRIVER={ODBC Driver 11 for SQL Server};SERVER=localhost;DATABASE=bike_crime;Trusted_Connection=yes'
+connStr = 'DRIVER={ODBC Driver 11 for SQL Server};SERVER=localhost;DATABASE=Road_Info;Trusted_Connection=yes'
 conn = pyodbc.connect(connStr)
 cursor = conn.cursor()
 
-Main_field = list("region", "srcdetail", "areaNm", "UID",
+Main_field = ("region", "srcdetail", "areaNm", "UID",
                   "direction", "y1", "happentime", "roadtype", "road", "modDttm",
                   "comment", "happendate", "x1")
 
 url = 'https://od.moi.gov.tw/MOI/v1/pbs'
 
+def check_sid():
+    sql = 'select * from Road_Info_main order by sid desc'
+    cursor.execute(sql)
+    row = cursor.fetchone()
+    if not row:
+        return 0
+    else:
+        return row.sid
+'''      
+class Info:
+    def _init_(self,region,srcdetail,areaNm,UID,direction,y1,happentime,roadtype,road,modDttm,comment,happendate,x1):
+        self.region = region
+        self.srcdetail=srcdetail
+        self.areaNm = areaNm
+'''       
+    
+
+def Insertdata(data):
+    sid = check_sid()
+    sql = 'insert into Road_Info_main values (?,?,?,?,?,?,?,?,?,?,?,?,?,?)'
+    for i in range(0,len(data)):
+        sid+=1
+        row = {}
+        row[0] = data[i]['region']
+        row[1] = data[i]['srcdetail']
+        row[2] = data[i]['areaNm']
+        row[3] = data[i]['UID']
+        row[4] = data[i]['direction']
+        row[5] = data[i]['y1']
+        row[6] = data[i]['happentime'] 
+        row[7] = data[i]['roadtype']
+        row[8] = data[i]['road']
+        row[9] = data[i]['modDttm']
+        row[10] = data[i]['comment'] #忽略上限太高
+        row[11] = data[i]['happendate']
+        row[12] = data[i]['x1']
+        # max = 0 算最大值
+        # if max < row[10]:
+        #     max = row[10]
+        # print(max)
+        cursor.execute(sql,sid,row[0],row[1],row[2],row[3],row[4],row[5],row[6],row[7],row[8],row[9],row[10],row[11],row[12])
+        conn.commit()
+    conn.close()
 
 
+    
 data = crawlerNewData(url)
-print(data)
+
+
+Insertdata(data)
+
+
 
 #def insertData(data):
 
--- main.py
+++ main.py
@@ -1,17 +1,18 @@
-from flask import Flask
+from flask_bootstrap import Bootstrap
 from config import DevConfig
-from crawler import crawler
+from flask import Flask,render_template
+
+
+
+
+
 app = Flask(__name__)
 app.config.from_object(DevConfig)
+bootstrap = Bootstrap(app)
 
-@app.route('/')
-def index():
-    return 'Hello World!'
-
-
-url = 'https://od.moi.gov.tw/MOI/v1/pbs'
-
-
+@app.route('/test')
+def test():
+    return render_template('test.html')
 
 
 if __name__ == '__main__':
