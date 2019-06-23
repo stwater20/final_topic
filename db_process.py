f4016f7546b53a0c1d8540493e848314ef9d9603
陳勝舢 <54sakuramiku@gmail.com>
2019-06-23 17:21:29 +0800

    add code

--- db_process.py
+++ db_process.py
@@ -2,6 +2,10 @@ import pyodbc
 import sys
 import json
 import requests
+#gmaps = googlemaps.Client(key='GOOGLE_API_KEY', queries_per_second=50, retry_over_query_limit=False)
+#Handling google map API quota exception
+
+
 
 
 def crawlerNewData(url):
@@ -31,26 +35,68 @@ Main_field = ("region", "srcdetail", "areaNm", "UID",
 
 url = 'https://od.moi.gov.tw/MOI/v1/pbs'
 
+
+
+def search_fun1(search,sort,limit):
+    sql = 'select TOP ' + str(limit) + ' * from Road order by ' + search \
+        + ' ' + sort 
+    cursor.execute(sql)
+    rows = cursor.fetchall()  #記得要for row in rows
+    return rows
+
+
+def search_fun2(search):
+    sql = 'select count(*), ' + search + ' from Road group by ' + search
+    cursor.execute(sql)
+    rows = cursor.fetchall()  # 記得要for row in rows
+    return rows
+
+def search_fun3(a,b1,b2,c1,c2):
+    # sql = 'select count(*), ' +  a + ' from Road where \
+    # substring(happendate, 6, 2)  between ' + b2 + ' and '+ c2 +' \
+    # and substring(happendate,1,4) between ' + b1 + ' and ' + c1 + ' group by ' + a
+    sql = 'select count(*), ' + str(a) +' from Road where \
+    substring(happendate, 6, 2)  between ? and ?  \
+    and substring(happendate,1,4) = ? or substring(happendate,1,4)= ? group by ' + str(a)
+    cursor.execute(sql,b2,c2,b1,c1)
+    rows = cursor.fetchall()  # 記得要for row in rows
+    return rows
+
+
+#以後有錢再說 one request per a day
+def google_Geocoding(rows):
+    ids = []
+    for row in rows:
+        geocode_result = gmaps.geocode(row.areaNm)
+        loc = geocode_result[0]['geometry']['location']
+        print("以" + row.areaNm + "為中心的半徑25000公尺的休息站數量：" + str(len(gmaps.places_radar(keyword="休息站",
+                                                                                    location=loc, radius=25000)['result'])))
+        for place in gmaps.places_radar(keyword="休息站",location=loc, radius=25000)['result']:
+            ids.append(place['place_id'])
+        break
+    return ids
+
+
+    
 def check_sid():
-    sql = 'select * from Road_Info_main order by sid desc'
+    sql = 'select * from Road order by sid desc'
     cursor.execute(sql)
     row = cursor.fetchone()
     if not row:
         return 0
     else:
-        return row.sid
-'''      
-class Info:
-    def _init_(self,region,srcdetail,areaNm,UID,direction,y1,happentime,roadtype,road,modDttm,comment,happendate,x1):
-        self.region = region
-        self.srcdetail=srcdetail
-        self.areaNm = areaNm
-'''       
-    
+        return row.sid  
+        
+def check_last_Repeating():
+    sql = 'select top 1 * from Road order by sid desc'
+    cursor.execute(sql)
+    row = cursor.fetchone()
+    return row.modDttm
 
 def Insertdata(data):
     sid = check_sid()
-    sql = 'insert into Road_Info_main values (?,?,?,?,?,?,?,?,?,?,?,?,?,?)'
+    check_modDttm = check_last_Repeating()
+    sql = 'insert into Road values (?,?,?,?,?,?,?,?,?,?,?,?,?,?)'
     for i in range(0,len(data)):
         sid+=1
         row = {}
@@ -71,18 +117,21 @@ def Insertdata(data):
         # if max < row[10]:
         #     max = row[10]
         # print(max)
-        cursor.execute(sql,sid,row[0],row[1],row[2],row[3],row[4],row[5],row[6],row[7],row[8],row[9],row[10],row[11],row[12])
+        if check_modDttm ==row[9]:
+           break 
+        cursor.execute(sql,sid,row[0],row[1],row[2],row[3],
+        row[4],row[5],row[6],row[7],row[8],row[9],row[10],row[11],row[12])
         conn.commit()
-    conn.close()
+    # conn.close()
 
 
     
 data = crawlerNewData(url)
+#Insertdata(data)
+#search_fun1("region", "desc", 1)
+#Insertdata(data)
+# t = search_fun3("roadtype", "2018", "01","2019","08")
+# for l in t:
+#     print(l)
 
-
-Insertdata(data)
-
-
-
-#def insertData(data):
 
--- main.py
+++ main.py
@@ -1,18 +1,102 @@
 from flask_bootstrap import Bootstrap
 from config import DevConfig
-from flask import Flask,render_template
-
-
-
-
+from flask import Flask, render_template, request, jsonify
+from db_process import Main_field, search_fun1, crawlerNewData, check_sid, check_last_Repeating, Insertdata,search_fun2,search_fun3
+import datetime
+import random
 
 app = Flask(__name__)
 app.config.from_object(DevConfig)
 bootstrap = Bootstrap(app)
 
-@app.route('/test')
+t1 = "region"
+t2 = "desc"
+
+@app.route('/')
 def test():
     return render_template('test.html')
+
+
+@app.route('/setData/')  # 路由
+def setData():
+    # now = datetime.datetime.now().strftime('%H:%M:%S')
+    a = request.args.get('a',0,type=str)
+    road = search_fun2("roadtype")
+    temp = []
+    for rd in road:
+        temp.append(rd[0])
+    data = { 'data': temp}
+    return jsonify(data)  # 將數據以字典的形式傳回
+
+@app.route('/t2')
+def t2():
+    return render_template('test2.html')
+
+
+@app.route('/t3')
+def t3():
+    return render_template('test3.html')
+
+@app.route('/at2')
+def at2():
+    a = request.args.get('a', 0, type=str)
+    b1 = request.args.get('b', 0, type=str)
+    b2 = request.args.get('c', 0, type=str)
+    c1 = request.args.get('d', 0, type=str)
+    c2 = request.args.get('e', 0, type=str)
+    print(a,b1,b2,c1,c2)
+    road = search_fun3(a,b1,b2,c1,c2)
+    temp1 = []
+    temp2 = []
+    for rd in road:
+        temp1.append(rd[0])
+        temp2.append(rd[1])
+    data = {'data1':temp1,'data2':temp2 ,'data3':a}
+    return jsonify(data) 
+
+@app.route('/_add_numbers')
+def add_numbers():
+    a = request.args.get('a', 0, type=str)
+    b = request.args.get('b', 0, type=str)
+    c = request.args.get('c', 0, type=str)
+    road = search_fun1(a,b,c)
+    output = "  <table class='table table-hover'><tr><td>路況區域</td>"
+    output += "<td>資料來源</td>"
+    output += "<td>地區區分說明</td>"
+    # output += "<td>唯一編號</td>"
+    output += "<td>方向</td>"
+    # output += "<td>緯度</td>"
+    output += "<td>發生時間</td>"
+    output += "<td>路況類別</td>"
+    output += "<td>道路名稱</td>"
+    # output += "<td>修改時間</td>"
+    output += "<td>路況說明</td>"
+    output += "<td>發生時間</td></tr>"
+    # output += "<td>經度</td></tr>"
+
+    for rd in road:
+        output = output \
+        + "<tr><td>" + rd.region + "</td>" \
+        + "<td>" + rd.srcdetail + "</td>" \
+        + "<td>" + rd.areaNm + "</td>" \
+        + "<td>" + rd.direction + "</td>" \
+        + "<td>" + rd.happentime + "</td>" \
+        + "<td>" + rd.roadtype + "</td>" \
+        + "<td>" + rd.road + "</td>" \
+        + "<td>" + rd.commemt + "</td>" \
+        + "<td>" + rd.happendate + "</td></tr>" 
+    output+="</table>"
+    return jsonify(result=output)
+
+
+@app.route("/demo", methods=["POST", "GET"])
+def demo():
+    nick_name = request.form.get("username")
+    print(nick_name)
+    return "ok"
+
+
+
 
 
 if __name__ == '__main__':
